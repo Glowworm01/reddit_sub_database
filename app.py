9af528b0e0dd9676c3f0b9bbddb7d509a9bf4407
Benjamin Berghofer <bberghofer1004@gmail.com>
2021-02-28 00:12:54 +1000

    basic file saving

--- app.py
+++ app.py
@@ -13,20 +13,22 @@ subs = {}
 def add_to_db(sub):
     name = sub.display_name
     desc = sub.public_description
-    subs[name] = desc
+    users = sub.subscribers
+    subs[name] = [users, desc]
     with open("sub_db.json", "w") as fl:
         json.dump(subs, fl, indent=4)
 
 def print_sub(sub):
     name = colored(sub.display_name, 'green', attrs=['bold'])
     desc = colored(sub.public_description, 'blue')
+    users = colored(sub.subscribers, 'red')
     raw_url = 'https://www.reddit.com' + sub.url
     url = colored(raw_url, 'magenta')
-    print('\n' + name + ': ' + url + '\n' + desc)
+    print('\n' + name + ': ' + url + ' - ' + users + '\n' + desc)
     
 def main():
     while True:
-        sub = reddit.subreddit("randnsfw")
+        sub = reddit.subreddit("random")
         add_to_db(sub)
         print_sub(sub)
         

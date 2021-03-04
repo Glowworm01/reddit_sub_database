b772a7d59d580216c46f091625472b9c224fd08c
Benjamin Berghofer <bberghofer1004@gmail.com>
2021-03-04 19:12:34 +1000

    ux

--- verify-db.py
+++ verify-db.py
@@ -11,23 +11,26 @@ reddit = Reddit(
     user_agent=client_data['user_agent']
 )
 
-with open("sub_db.json", "r") as fl:
+with open("subs_formatted.json", "r") as fl:
     db = json.load(fl)
 
 clean_db = {}
+processed = 0
 
 def check_sub(sub):
     subreddit = reddit.subreddit(sub)
     try:
         verify = subreddit.description
         clean_db[subreddit.display_name] = subreddit.public_description
-        print(colored(f"The subreddit '{sub}' is valid.", 'green'))
+        print(colored(f"The subreddit '{sub}' is valid. ({len(db)-processed} left)", 'green'), )
+        
     except:
-        print(colored(f"The subreddit '{sub}' is invalid. Removing it from database.", 'red'))
-
+        print(colored(f"The subreddit '{sub}' is invalid. Removing it from database. ({len(db)-processed} left)", 'red'))
+        
 print(colored("This may take some time", 'cyan', attrs=['bold']))
 for sub in db:
+    processed += 1
     check_sub(sub)
 
-with open("sub_db.json", "w") as fl:
+with open("subs_formatted.json", "w") as fl:
     json.dump(clean_db, fl, indent=4)
